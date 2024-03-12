3623144354c359267841de348045c7970a0051cc
Laur4xl <107406784+Laur4xl@users.noreply.github.com>
2024-03-12 10:17:31 -0300

    xereca de mel

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; j++){
-  botoes[i].onclick = function{
-    botoes[i].classList.add("ativo")
+  botoes[i].onclick = function(){
+    for(let j = 0; j <botoes.length;j++)
+    botoes[j].classList.add("ativo");
 }    
+botoes[i].classList.add("ativo")
+}
+
 }
